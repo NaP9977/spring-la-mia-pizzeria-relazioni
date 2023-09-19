f36d1e6f734e56157bf1806d7680173cee110c37
NaP <annposillipo@gmail.com>
2023-09-19 20:22:06 +0200

    Almost_done

--- src/main/java/org/learning/java/pizzeria/model/Pizza.java
+++ src/main/java/org/learning/java/pizzeria/model/Pizza.java
@@ -1,7 +1,9 @@
 package org.learning.java.pizzeria.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
@@ -12,12 +14,15 @@ public class Pizza {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @NotBlank(message = "tutti i campi devono essere riempiti")
-    @Size(min = 10, max=100)
 
+    @NotBlank(message = "Scrivi il nome della pizza!")
     private String name;
-
+    @NotBlank(message = "Si prega di riempire tutti i campi del form" )
+    @Size(min = 10, max=100)
+    @Column(length = 100, nullable = false, unique = true)
     private String description;
+    @Min(0)
+    @NotNull
     private BigDecimal price;
 
     public Integer getId() {
